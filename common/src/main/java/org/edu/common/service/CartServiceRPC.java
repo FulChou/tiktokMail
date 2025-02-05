27fd7a60c9bbbedba8cc133e3ce891ac25321f26
zhoufu <46887649+CSU-FulChou@users.noreply.github.com>
2025-02-05 14:42:33 +0800

    补充购物车模块接口

--- common/src/main/java/org/edu/common/service/CartServiceRPC.java
+++ common/src/main/java/org/edu/common/service/CartServiceRPC.java
@@ -1,5 +1,21 @@
 package org.edu.common.service;
 
+import org.edu.common.domain.ProductDTO;
+
+import java.util.List;
+
 public interface CartServiceRPC {
+
+    // 购物车中有一系列商品：
+    List<ProductDTO> getCartList(Long userId);
+
+    // 添加商品到购物车：
+    void addProductToCart(Long userId, ProductDTO product);
+
+    // 从购物车中删除商品：
+    void deleteProductFromCart(Long userId, ProductDTO product);
+
+    // 清空购物车：
+    void clearCart(Long userId);
 
 }
